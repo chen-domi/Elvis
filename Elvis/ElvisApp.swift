f982df3c5bef1e1ad61f5fda3f939288be9219df
chen-domi <cchendominic@gmail.com>
2025-09-07 00:19:46 -0400

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by Dominic Chen on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
